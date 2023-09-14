ddcb11e831ff491ba609969d864744b460e1423c
Francisco Jose Moran Mato <fmoran-m@c2r16s2.42madrid.com>
2023-09-14 15:53:59 +0200

    reunión

---
+++ ft_memchr.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fmoran-m <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/14 15:51:28 by fmoran-m          #+#    #+#             */
+/*   Updated: 2023/09/14 15:53:43 by fmoran-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <string.h>
+//#include <stdio.h>
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	size_t		i;
+	const char	*str;
+
+	str = s;
+	i = 0;
+	while (*str != '\0' && i < (n - 1) && *str != c)
+	{
+		str++;
+		i++;
+	}
+	if (*str != c || i == 0)
+		return (0);
+	else
+		return ((char *)str);
+}
+/*
+int main(void)
+{
+	char *s = "Hola";
+	char *p = ft_memchr(s, 'z', 2);
+	printf("%s\n", p);
+	return 0;
+}
+*/
