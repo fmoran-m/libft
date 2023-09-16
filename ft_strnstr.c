93686684458e2697868d69160ab46e3ecb9c0357
Fran <franmoranmato@gmail.com>
2023-09-16 19:54:43 +0200

    strnstr

---
+++ ft_strnstr.c
@@ -0,0 +1,39 @@
+//#include <string.h>
+//#include <stdio.h>
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t	i;
+	size_t	k;
+	size_t	temp;
+
+	i = 0;
+	k = 0;
+	if (*needle == '\0')
+		return ((char *)haystack);
+	while (*haystack != '\0' && i < len)
+	{
+		temp = i;
+		while (haystack[i] && needle[k] && haystack[i] == needle[k] && i < len)
+		{
+			i++;
+			k++;
+		}
+		if (needle[k] == '\0')
+			return ((char *)haystack + temp);
+		i = temp;
+		k = 0;
+		i++;
+	}
+	return (0);
+}
+/*
+int main (void)
+{
+        char *a = "Hola";
+        char *b = "";
+        char *c = ft_strnstr(a, b, 4);
+        printf("%s\n", c);
+        return 0;
+}
+*/
