c34d0bf903c37d88edcae4d92c25f9bb727f6eaa
Francisco Jose Moran Mato <fmoran-m@c2r4s3.42madrid.com>
2023-09-18 16:57:57 +0200

    substr

--- ft_substr.c
+++ ft_substr.c
@@ -1,72 +1,61 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fmoran-m <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/18 15:21:45 by fmoran-m          #+#    #+#             */
-/*   Updated: 2023/09/18 16:56:33 by fmoran-m         ###   ########.fr       */
+/*   Created: 2023/09/18 16:42:05 by fmoran-m          #+#    #+#             */
+/*   Updated: 2023/09/18 16:48:09 by fmoran-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 //#include <stdlib.h>
 //#include <stdio.h>
 
-size_t	ft_strlen(const char *s)
+char	*pointer(char const *s, unsigned int start)
 {
-	int	i;
-
-	i = 0;
-	while (s[i])
-		i++;
-	return (i);
-}
-
-char	*new(char const *s1, char const *s2)
-{
-	size_t	len1;
-	size_t	len2;
-	char	*ptr;
-
-	len1 = ft_strlen(s1);
-	len2 = ft_strlen(s2);
-	ptr = (char *)malloc(((len1 + len2) * sizeof(char)) + 1);
-	return (ptr);
-}
-
-char	*ft_strjoin(char const *s1, char const *s2)
-{
-	char	*ptr;
 	size_t	i;
 	size_t	k;
+	char	*new;
 
 	i = 0;
 	k = 0;
-	ptr = new(s1, s2);
-	if (ptr == 0)
-		return (0);
-	while (s1[i])
-	{
-		ptr[i] = s1[i];
+	while (i < start)
 		i++;
-	}
-	while (s2[k])
+	while (s[i])
 	{
-		ptr[i] = s2[k];
-		i++;
 		k++;
+		i++;
 	}
-	ptr[i] = '\0';
-	return (ptr);
+	new = (char *)malloc(k * sizeof(char) + 1);
+	return (new);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	size_t	i;
+	char	*new;
+
+	new = pointer(s, start);
+	if (new == 0)
+		return (0);
+	i = 0;
+	while (s[start] && i < len)
+	{
+		new[i] = s[start];
+		i++;
+		start++;
+	}
+	new[i] = '\0';
+	return (new);
 }
 /*
 int main (void)
 {
 	char a[20] = "Hola";
-	char b[20] = "Adios";
-	char *c = ft_strjoin(a, b);
-	printf("%s\n", c);
+	char *b = ft_substr(a, 0, 6);
+	printf("%s\n", b);
 	return 0;
 }
 */
