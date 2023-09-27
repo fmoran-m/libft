25c8107cd33bfcc8973bc9964fd2c5366ef608a7
Francisco Jose Moran Mato <fmoran-m@c2r16s2.42madrid.com>
2023-09-27 15:46:19 +0200

    .

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: fmoran-m <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/25 13:50:00 by fmoran-m          #+#    #+#             */
-/*   Updated: 2023/09/27 14:28:24 by fmoran-m         ###   ########.fr       */
+/*   Updated: 2023/09/27 15:45:59 by fmoran-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,6 +36,21 @@ size_t	count_word(char const *s, unsigned char ca)
 	return (n);
 }
 
+char	**reserve_memory(char const *s, char c, size_t n_rows)
+{
+	char	**ptr;
+	size_t	i;
+	size_t	j;
+
+	ptr = malloc((sizeof(char*) * n_rows) + 1);
+	if (ptr == NULL)
+		return (NULL);
+	while (s[i])
+	{ 
+		i++;
+
+
+/*
 char	**allocate_array(char const *s, unsigned char ca, char **ptr, size_t n_rows)
 {
 	size_t i;
@@ -59,12 +74,14 @@ char	**allocate_array(char const *s, unsigned char ca, char **ptr, size_t n_rows
 	ptr[j] = 0;
 	return (ptr);
 }
-
-void	ft_split(char const *s, char c)
+*/
+char	**ft_split(char const *s, char c)
 {
 	size_t 			n_rows;
+	char			**ptr;
 
 	s = ft_strtrim(s, &c);
 	n_rows = count_word(s, c);
+	ptr = reserve_memory(s, c, n_rows);
 	printf("%zu\n", n_rows);
 }
