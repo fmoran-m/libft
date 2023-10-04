cc683e44e1484f5addbf1f3639e133a4f54b6d04
Your Name <franmoranmato@gmail.com>
2023-10-05 01:19:10 +0200

    Main de lst

--- ft_lstiter.c
+++ ft_lstiter.c
@@ -1,4 +1,4 @@
-/* ************************************************************************** */
+/* **********************(char *)**************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   ft_lstiter.c                                       :+:      :+:    :+:   */
@@ -22,3 +22,41 @@ void	ft_lstiter(t_list *lst, void (*f)(void *))
 		lst = lst->next;
 	}
 }
+
+void	f(void* a)
+{
+    int i = 0;
+    char *str;
+
+    str = a;
+    while(str[i]) 
+    {
+	str[i] = str[i] + 2;
+	i++;
+    }
+}
+/*
+int main (void)
+{
+    t_list *primero;
+    t_list *segundo;
+    t_list *tercero;
+    char *a = ft_strdup("AAA");
+    char *b = ft_strdup("HHH");
+    char *c = ft_strdup("MMM");
+
+    primero = ft_lstnew(a);
+    segundo = ft_lstnew (b);
+    tercero = ft_lstnew (c);
+    primero->next = segundo;
+    segundo->next = tercero;
+    printf("%s\n",(char *)primero->content);
+    printf("%s\n",(char *)segundo->content);
+    printf("%s\n",(char *)tercero->content);
+    ft_lstiter(primero, f);
+    printf("%s\n",(char *)primero->content);
+    printf("%s\n",(char *)segundo->content);
+    printf("%s\n",(char *)tercero->content);
+return 0; 
+}
+*/
